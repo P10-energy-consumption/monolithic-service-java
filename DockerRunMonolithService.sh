788720c7e04634297c97608c4bd9aed1a1b06e9e
benchy-data <cs-21-pt-9-01@hotmail.com>
2022-04-05 11:37:00 +0000

    Test setup v2

--- DockerRunMonolithService.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd PetStore && mvn clean install && docker build -t petstore-monolith-java .
-
-docker run --rm --net=host -p 8080:8080 petstore-monolith-java &
-wait
--- jmeter_start.sh
+++
@@ -1,1 +0,0 @@
-/home/usr/Downloads/apache-jmeter-5.4.3/bin/jmeter -n -t "/home/usr/monolithic-service-java/Monolith Load Testing.jmx"
