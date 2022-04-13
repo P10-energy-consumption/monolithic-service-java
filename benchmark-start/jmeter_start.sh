bd166d44c22d84503328d162b657809d41c542b3
GarridoY <dgmn17@student.aau.dk>
2022-04-13 11:45:34 +0200

    Thread Count Ramp-Up + Hardware monitor

--- benchmark-start/jmeter_start.sh
+++ benchmark-start/jmeter_start.sh
@@ -1,3 +1,16 @@
-sleep 300
-/home/usr/Downloads/apache-jmeter-5.4.3/bin/jmeter -n -t "/home/usr/energy-consumption/java/monolithic-service-java/benchmark-start/Monolith Load Testing.jmx"
-sleep 300
+#!/bin/bash
+
+clients=7200
+setup_teardown=30
+idle=300
+end=$((SECONDS+$idle+$clients+$idle+$setup_teardown))
+echo "Begin collecting Docker stats now for $end seconds."
+while [ $SECONDS -lt $end ]; do docker stats --no-stream | tee -a stats_docker_monolith.txt; sleep 1; done &
+
+echo "Sleeping for $idle seconds before starting JMeter test..."
+sleep "${idle}"
+echo "Starting JMeter test plan"
+/home/usr/Downloads/apache-jmeter-5.4.3/bin/jmeter -n -t "LoadTesting.jmx"
+echo "Sleeping for $idle seconds before ending benchmark..."
+sleep "${idle}"
+echo "jmeter_start.sh done"
