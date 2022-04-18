ef60db48eef84c3e0635eb8a579d377563eb743f
GarridoY <dgmn17@student.aau.dk>
2022-04-18 12:56:29 +0200

    Singletons instead of Instance-per-request

--- PetStore/src/main/java/org/p10/PetStore/PetStoreApplication.java
+++ PetStore/src/main/java/org/p10/PetStore/PetStoreApplication.java
@@ -1,9 +1,23 @@
 package org.p10.PetStore;
 
+import org.p10.PetStore.Controllers.PetController;
+import org.p10.PetStore.Controllers.StoreController;
+import org.p10.PetStore.Controllers.UserController;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("")
 public class PetStoreApplication extends Application {
 
+    @Override
+    public Set<Object> getSingletons() {
+        Set<Object> set = new HashSet<>();
+        set.add(new PetController());
+        set.add(new StoreController());
+        set.add(new UserController());
+        return set;
+    }
 }
